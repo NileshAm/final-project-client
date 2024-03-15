a1290e88fd2186744d3e746bd35f3fe2470d03d1
Nilesh Amarathunge <nileshamarathunge@gmail.com>
2024-03-15 10:10:55 +0530

    product card component modified

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,30 +1,69 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./ProductCard.css";
 
-import img from "../../temp/Galaxy-S23-Ultra-Green.jpg";
 import StarRating from "../StarRating/StarRating";
-const ProductCard = () => {
+const ProductCard = ({ data }) => {
+  let { name, rating, price, discount, img, url } = data;
+
+  const navigate = useNavigate();
   return (
-    <div className="col-12 col-sm-4 col-md-3 col-lg-2 border p-2 rounded-3 shadow-sm product-card" onClick={()=>{alert("redirect")}}>
-      <div className="border rounded w-100">
+    <div
+      className="col-12 col-sm-4 col-md-3 col-lg-2 border p-2 rounded-3 shadow-sm product-card m-1"
+      onClick={() => {
+        navigate(`/product?id=${url}`);
+      }}
+    >
+      <div className="border rounded w-100 img-back">
         <img src={img} alt="Product" className="img" />
       </div>
-      <div className="w-100 fs-5">Samsung Galxy S23 Ultra</div>
-      <StarRating Rating={2.5} starSize={15} className="rating-width" />
+      <div className="w-100 fs-5">{name}</div>
+      <StarRating Rating={rating} starSize={15} className="rating-width" />
       <div className="row">
         <div className="col-6 fw-bold text-success ">
           LKR.
           <br />
-          300,000.00
+          {discount ? (price * (100 - discount)) / 100 : price}
         </div>
-        <div className="col">
-          <div className="col-6 pt-1 fs-8 fw-bold text-danger ">
-            LKR.30,000.00
+        {discount !== 0 ? (
+          <div className="col">
+            <div className="col-6 pt-1 fs-8 fw-bold text-danger ">
+              LKR.{discount && (price * discount) / 100}
+            </div>
+            <div >
+
+        <svg
+          width={60}
+          viewBox="0 0 252 96"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{position:"absolute"}}
+        >
+          <g id="Group 1">
+            <g id="Label">
+              <ellipse
+                id="Ellipse 1"
+                cx="32.5529"
+                cy="48.0207"
+                rx="31.722"
+                ry="47.1763"
+                fill="#F52A2A"
+              />
+              <path
+                id="Subtract"
+                d="M196.856 48.0207L251.903 0.844406H32.5529V95.197H251.903L196.856 48.0207Z"
+                fill="#F52A2A"
+              />
+            </g>
+          </g>
+        </svg>
+        <p style={{position:"absolute"}} className="fs-7 fw-bolder text-light ps-2" >-{discount}%</p>
+        </div>
           </div>
-          <div className="bg-danger text-light d-flex justify-content-center my-2 mx-2 rounded-2 shadow-sm fs-7 fw-bolder ">-10%</div>
-        </div>
+        ) : null}
+        
       </div>
     </div>
   );
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import ProductCard from '../../Components/ProductCard/ProductCard'
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import ProductCard from "../../Components/ProductCard/ProductCard";
+import axios from "axios";
 
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
 
 const HomePage = () => {
+  const [data, setData] = useState([]);
+
+  useLayoutEffect(()=>{
+    axios
+      .get("http://localhost:5000/home")
+      .then((res) => {
+        setData(res.data)
+      })
+
+    }, [])
+
+    useEffect(()=>console.log(data), [data])
   return (
     <>
-    <main className='container-fluid'>
-      <ProductCard/>
-    </main>
+      <main className="container-fluid">
+        <div className="row d-flex justify-content-around m-2">
+          {data.map((v, k) => {
+            return <ProductCard data={v} key={k} />;
+          })}
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
