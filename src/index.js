8d592dc8fee5ca769759a0483ced86e36b55f51b
Nilesh Amarathunge <nileshamarathunge@gmail.com>
2024-03-29 17:13:17 +0530

    searchPanel, search page made; app.js modified for searchPanel to be rendered in all pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
