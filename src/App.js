004d34fde5459395d65effee9ffb53b158c5874d
SenithuJkdy <senithujaykody310@gmail.com>
2024-04-26 18:29:20 +0530

    ProductView initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import About from "./Pages/About/About";
 import CheckAdminLogin from "./Components/CheckAdminLogin/CheckAdminLogin";
+import ProductView from "./Pages/ProductView/ProductView";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/product" element={<ProductView />} />
         <Route path="/cart" element={<h1>cart</h1>} />                  
         <Route path="/search" element={<h1>search</h1>} />
         <Route path="/login" element={<h1>Login</h1>} />
